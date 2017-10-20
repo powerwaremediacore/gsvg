ea801f4b190e84c5ba93b497a80558f1e2b7899b
Daniel Espinosa <daniel.espinosa@pwmc.mx>
2017-10-20 12:11:13 -0500

    Imporved example on RectElement in Python

--- examples/python/rect.py
+++ examples/python/rect.py
@@ -3,7 +3,10 @@ gi.require_version ('GSvg','0.4')
 from gi.repository import Gio, GSvg
 
 svg = GSvg.GsSvg ()
-r = svg.create_rect ("0cm", "0cm", "9mm","9mm", None, None)
+r = svg.create_rect ("0mm", "0mm", "20mm","20mm", None, None)
+r.set_fill("none")
+r.set_stroke ("black")
+r.set_stroke_width ("1mm")
 svg.append_child (r)
 
 f = Gio.File.new_for_path ("rect.svg")
